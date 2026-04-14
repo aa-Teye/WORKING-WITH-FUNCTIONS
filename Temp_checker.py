0c7f4f965f1167d526eaa12834b4808b7a06773d
aa-Teye <alexteyeametepey@gmail.com>
2026-04-14 23:28:52 +0000

    Organize exercise2

--- Temp_checker.py
+++ Temp_checker.py
@@ -3,6 +3,6 @@
 def check_temperature(temp:float):
     return temp
 
-
+# phase2 Get user input and call the function
 temp = float(input("Enter your temperature:  "))
 result = check_temperature(temp)
