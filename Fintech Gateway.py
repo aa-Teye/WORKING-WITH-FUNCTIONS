963a187a86e6fcb95e30e736bbcfd267b303cc84
aa-Teye <alexteyeametepey@gmail.com>
2026-04-15 22:28:15 +0000

    updating the main engine

--- Fintech Gateway.py
+++ Fintech Gateway.py
@@ -1,6 +1,6 @@
 def calculate_levy(amount:float):
     if amount > 10000: 
-        return "You have exceeded the amount limit for this transaction"
+        return -1.0
     if amount > 100:
         return (amount * 0.01) + 2
     elif amount < 100:
@@ -12,4 +12,9 @@ def calculate_levy(amount:float):
 
 amount = float(input("Enter the amount: "))
 result = calculate_levy(amount)
-print(f"The levy for this transaction is : {result}")
+
+if result == -1.0:
+    print("Transaction amount exceeds the limit for levy calculation.")
+else:
+    print(f"The levy for this transaction is: {result}")
+
