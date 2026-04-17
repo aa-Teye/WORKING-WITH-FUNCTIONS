bc6de75f44903e5494accde85b63305b778b0a43
aa-Teye <alexteyeametepey@gmail.com>
2026-04-17 23:59:27 +0000

    updating new function

--- HealthRiskCalculator.py
+++ HealthRiskCalculator.py
@@ -3,17 +3,3 @@ def calculate_risk(age: float, multiplier: float = 1.5, max_age: float = 120.0):
     if age > max_age:
         return -1.0
     
-    # 2. Then, we do the math and return the result
-    risk_score = age * multiplier
-    return risk_score
-
-# --- Execution ---
-user_age = float(input("Enter patient age: "))
-
-# We call the function. It will use 1.5 and 120.0 automatically!
-result = calculate_risk(user_age)
-
-if result == -1.0:
-    print("Invalid age entered.")
-else:
-    print(f"Patient Risk Score: {result}")
